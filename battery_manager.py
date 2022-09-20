5693dab2cdb6aa70e5b50ba9a5c49d8ab2393980
Lukas Koch <lukas.koch@mailbox.org>
2022-09-20 12:18:42 +0200

    Disable logging of the charge plan.

--- battery_manager.py
+++ battery_manager.py
@@ -92,7 +92,6 @@ class BatteryManager(hass.Hass):
         if self.publish:
             index, values = zip(*steps)
             series = pd.Series(values, index)
-            self.log(series)
             self.global_vars[self.publish] = series
 
         if len(steps) < 2:

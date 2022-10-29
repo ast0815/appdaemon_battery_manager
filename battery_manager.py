81f55b2d533bee64fdd9e8881cac147f6ecd7116
Lukas Koch <lukas.koch@mailbox.org>
2022-10-29 17:05:12 +0200

    Debug prices failure.

--- battery_manager.py
+++ battery_manager.py
@@ -88,6 +88,7 @@ class BatteryManager(hass.Hass):
         # Charge state last time we looked
         self.last_charge = int(await self.get_state(self.charge_state_entity))
         prices = self.global_vars.get("electricity_prices", None)
+        self.log(prices)
         if prices is None or len(prices) == 0:
             self.last_time = pd.Timestamp.now(tz="UTC")
         else:
